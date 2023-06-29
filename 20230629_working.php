4daf1de94029f424153ca3c4f59edc8ded2a70a4
Seongtea Nam <nst3644@kakao.com>
2023-06-29 13:27:04 +0900

    D1

--- 20230629_working.php
+++ 20230629_working.php
@@ -1,1 +1,8 @@
-# 조직도 변경 
+# 조직도 변경 1차 카테고리 적용 기획
+
+############################################
+기자조회 및 검색
+1. View = Information/reporterlist 
+2. Controllers = Information/Reporter 
+3. Model = Information/Information_medel
+############################################
